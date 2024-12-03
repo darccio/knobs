c2870417a30d987fdd1d7420029473570264b569
Dario Castañé <d@rio.hn>
2024-12-03 11:59:14 +0100

    feat(tests): internalize tests

--- knob_test.go
+++ knob_test.go
@@ -1,107 +1,128 @@
-package knobs_test
+package knobs
 
 import (
 	"fmt"
 	"testing"
 
-	"github.com/darccio/knobs"
 	"github.com/stretchr/testify/require"
 )
 
 func TestRegister(t *testing.T) {
 	t.Parallel()
-	def := &knobs.Definition[string]{
+
+	def := &Definition[string]{
 		Default: "default",
 	}
-	knob := knobs.Register(def)
+	knob := Register(def)
 
-	value := knobs.Get(knob)
+	value := Get(knob)
 	require.Equal(t, "default", value)
 }
 
 func TestInitialize(t *testing.T) {
-	def := &knobs.Definition[string]{
+	def := &Definition[string]{
 		Default: "default",
-		EnvVars: []string{"TEST_KNOB"},
+		EnvVars: []string{"TEST_KNOB_INIT"},
 	}
 
 	t.Run("no env var", func(t *testing.T) {
-		knob := knobs.Register(def)
+		knob := Register(def)
 
-		value := knobs.Get(knob)
+		value := Get(knob)
 		require.Equal(t, "default", value)
 	})
 
 	t.Run("env var", func(t *testing.T) {
-		t.Setenv("TEST_KNOB", "env value")
-		knob := knobs.Register(def)
+		t.Setenv("TEST_KNOB_INIT", "env value")
+		knob := Register(def)
 
-		value := knobs.Get(knob)
+		value := Get(knob)
 		require.Equal(t, "env value", value)
 	})
 }
 
 func TestCleanEnvvar(t *testing.T) {
-	t.Setenv("TEST_KNOB", "env value")
-	def := &knobs.Definition[string]{
+	t.Setenv("TEST_KNOB_CLEAN", "env value")
+
+	def := &Definition[string]{
 		Default: "default",
-		EnvVars: []string{"TEST_KNOB"},
+		EnvVars: []string{"TEST_KNOB_CLEAN"},
 		CleanEnvvar: func(v string) string {
 			return fmt.Sprintf("cleaned: %s", v)
 		},
 	}
-	knob := knobs.Register(def)
+	knob := Register(def)
 
-	value := knobs.Get(knob)
+	value := Get(knob)
 	require.Equal(t, "cleaned: env value", value)
 }
 
 func TestSet(t *testing.T) {
 	t.Parallel()
-	def := &knobs.Definition[string]{
+
+	def := &Definition[string]{
 		Default: "default",
 	}
-	knob := knobs.Register(def)
+	knob := Register(def)
 
 	t.Run("set by known origin", func(t *testing.T) {
-		knobs.Set(knob, "new value", knobs.Code)
+		Set(knob, "new value", Code)
 
-		value := knobs.Get(knob)
+		value := Get(knob)
 		require.Equal(t, "new value", value)
 	})
 
 	t.Run("not set an unknown origin", func(t *testing.T) {
-		knobs.Set(knob, "known value", knobs.Code)
-		knobs.Set(knob, "this shouldn't be", knobs.Env) // this fails silently
+		Set(knob, "known value", Code)
+		Set(knob, "this shouldn't be", Env) // this fails silently
 
-		value := knobs.Get(knob)
+		value := Get(knob)
 		require.Equal(t, "known value", value)
 	})
 }
 
 func TestDerive(t *testing.T) {
-	def := &knobs.Definition[string]{
+	t.Parallel()
+
+	def := &Definition[string]{
 		Default: "default",
 	}
-	parent := knobs.Register(def)
-	knob := knobs.Derive(parent)
+	parent := Register(def)
+	knob := Derive(parent)
 
 	t.Run("parent default value", func(t *testing.T) {
-		value := knobs.Get(knob)
+		value := Get(knob)
 		require.Equal(t, "default", value)
 	})
 
 	t.Run("parent set value", func(t *testing.T) {
-		knobs.Set(parent, "parent value", knobs.Code)
+		Set(parent, "parent value", Code)
 
-		value := knobs.Get(knob)
+		value := Get(knob)
 		require.Equal(t, "parent value", value)
 	})
 
 	t.Run("knob set value", func(t *testing.T) {
-		knobs.Set(knob, "knob value", knobs.Code)
+		Set(knob, "knob value", Code)
 
-		value := knobs.Get(knob)
+		value := Get(knob)
 		require.Equal(t, "knob value", value)
 	})
+}
+
+func TestDeleteState(t *testing.T) {
+	t.Parallel()
+
+	knob := Register(&Definition[string]{
+		Default: "default",
+	})
+
+	ref := int(knob)
+	_, ok := registry[ref]
+	require.True(t, ok)
+
+	deleteState(ref)
+
+	_, ok = registry[ref]
+	require.False(t, ok)
 }
